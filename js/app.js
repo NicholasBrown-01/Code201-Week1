9d87cd5ed110d0c5c7967831b25828c900669941
Nicholas <nicholasbrown.us@gmail.com>
2023-02-08 18:34:38 -0500

    Lab03 Complete

--- js/app.js
+++ js/app.js
@@ -1,228 +1,228 @@
-// //DECLARE STRICT MODE TO PREVENT UNINTENTIONAL VARIABLES
-// 'use strict';
+//DECLARE STRICT MODE TO PREVENT UNINTENTIONAL VARIABLES
+'use strict';
 
 // Introduction Prompt
 let aName = '';
 
-// function userName() {
-//   aName = prompt('Welcome, what is your name?');
-// //   console.log('The user entered: ' + aName);
+function userName() {
+  aName = prompt('Welcome, what is your name?');
+//   console.log('The user entered: ' + aName);
 
-//   if (aName && aName.length >= 1) {
-//     window.alert('Wonderful to have you ' + aName + ', please feel free to read about me on this site.');
-//     window.alert('But first, let me ask you 7 fun questions you can guess about me.');
+  if (aName && aName.length >= 1) {
+    window.alert('Wonderful to have you ' + aName + ', please feel free to read about me on this site.');
+    window.alert('But first, let me ask you 7 fun questions you can guess about me.');
 
-//   } else if (!aName) {
-//     alert('You did not enter a valid name.');
-//     userName();
-//   }
+  } else if (!aName) {
+    alert('You did not enter a valid name.');
+    userName();
+  }
 
-// }
+}
 
-// userName();
+userName();
 
-// // Question 1: "Do I like Dogs?"
+// Question 1: "Do I like Dogs?"
 
-// function questionOne() {
-//   let answer = prompt('Do I like dogs? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
+function questionOne() {
+  let answer = prompt('Do I like dogs? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
 
-//   //   if (!answer) {
-//   //     alert('Please try your answer again');
-//   //   }
+  //   if (!answer) {
+  //     alert('Please try your answer again');
+  //   }
 
-//   let answerLower;
+  let answerLower;
 
-//   if (answer) {
-//     answerLower = answer.toLowerCase();
-//   }
+  if (answer) {
+    answerLower = answer.toLowerCase();
+  }
 
-//   if (answerLower === 'yes' || answerLower === 'y') {
-//     // console.log('Answer yes: ' + answerLower);
-//     alert('You are correct!');
+  if (answerLower === 'yes' || answerLower === 'y') {
+    // console.log('Answer yes: ' + answerLower);
+    alert('You are correct!');
 
-//   } else if (answerLower === 'no' || answerLower === 'n') {
-//     console.log('Answer no: ' + answerLower);
-//     alert('Sorry but I DO like dogs!');
+  } else if (answerLower === 'no' || answerLower === 'n') {
+    console.log('Answer no: ' + answerLower);
+    alert('Sorry but I DO like dogs!');
 
-//   } else {
-//     alert('Please try your answer again');
-//     questionOne();
-//   }
+  } else {
+    alert('Please try your answer again');
+    questionOne();
+  }
 
-// }
+}
 
-// questionOne();
+questionOne();
 
-// // Question 2: "Do I live in the city?"
+// Question 2: "Do I live in the city?"
 
-// function questionTwo() {
-//   let answer = prompt('Do I live in the city? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
+function questionTwo() {
+  let answer = prompt('Do I live in the city? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
 
-//   //   if (!answer) {
-//   //     alert('Please try your answer again');
-//   //     questionTwo();
-//   //   }
-//   let answerLower;
+  //   if (!answer) {
+  //     alert('Please try your answer again');
+  //     questionTwo();
+  //   }
+  let answerLower;
 
-//   if (answer) {
-//     answerLower = answer.toLowerCase();
-//   }
+  if (answer) {
+    answerLower = answer.toLowerCase();
+  }
 
-//   if (answerLower === 'yes' || answerLower === 'y') {
-//     // console.log('Answer yes: ' + answerLower);
-//     alert('Nope! I live in the country on over 30 Acres!');
+  if (answerLower === 'yes' || answerLower === 'y') {
+    // console.log('Answer yes: ' + answerLower);
+    alert('Nope! I live in the country on over 30 Acres!');
 
-//   } else if (answerLower === 'no' || answerLower === 'n') {
-//     console.log('Answer no: ' + answerLower);
-//     alert('Correct! I live in the country on over 30 Acres!');
+  } else if (answerLower === 'no' || answerLower === 'n') {
+    console.log('Answer no: ' + answerLower);
+    alert('Correct! I live in the country on over 30 Acres!');
 
-//   } else {
-//     alert('Please try your answer again');
-//     questionTwo();
-//   }
+  } else {
+    alert('Please try your answer again');
+    questionTwo();
+  }
 
-// }
+}
 
-// questionTwo();
-
-
-
-// // Question 3: "Do I raise animals?"
-
-// function questionThree() {
-//   let answer = prompt('Do I raise animals? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
-
-//   //   if (!answer) {
-//   //     alert('Please try your answer again');
-//   //     questionThree();
-//   //   }
-
-//   let answerLower;
-
-//   if (answer) {
-//     answerLower = answer.toLowerCase();
-//   }
-
-//   if (answerLower === 'yes' || answerLower === 'y') {
-//     // console.log('Answer yes: ' + answerLower);
-//     alert('Absolutely! I have raised Pigs, Goats, Chickens, Turkeys, Cows, and Dogs!');
-
-//   } else if (answerLower === 'no' || answerLower === 'n') {
-//     console.log('Answer no: ' + answerLower);
-//     alert('Sorry! I have had several! Pigs, Goats, Chickens, Turkeys, Cows, and Dogs!');
-
-//   } else {
-//     alert('Please try your answer again');
-//     questionThree();
-//   }
-
-// }
-
-// questionThree();
+questionTwo();
 
 
-// // Question 4: "Do I have a family?"
 
-// function questionFour() {
-//   let answer = prompt('Do I have a family? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
+// Question 3: "Do I raise animals?"
 
-//   //   if (!answer) {
-//   //     alert('Please try your answer again');
-//   //     questionFour();
-//   //   }
+function questionThree() {
+  let answer = prompt('Do I raise animals? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
 
-//   let answerLower;
+  //   if (!answer) {
+  //     alert('Please try your answer again');
+  //     questionThree();
+  //   }
 
-//   if (answer) {
-//     answerLower = answer.toLowerCase();
-//   }
-//   if (answerLower === 'yes' || answerLower === 'y') {
-//     // console.log('Answer yes: ' + answerLower);
-//     alert('I sure do! I have an amazing wife and 4 children.');
+  let answerLower;
 
-//   } else if (answerLower === 'no' || answerLower === 'n') {
-//     console.log('Answer no: ' + answerLower);
-//     alert('Wrong! I have an amazing wife and 4 children.');
+  if (answer) {
+    answerLower = answer.toLowerCase();
+  }
 
-//   } else {
-//     alert('Please try your answer again');
-//     questionFour();
-//   }
+  if (answerLower === 'yes' || answerLower === 'y') {
+    // console.log('Answer yes: ' + answerLower);
+    alert('Absolutely! I have raised Pigs, Goats, Chickens, Turkeys, Cows, and Dogs!');
 
-// }
+  } else if (answerLower === 'no' || answerLower === 'n') {
+    console.log('Answer no: ' + answerLower);
+    alert('Sorry! I have had several! Pigs, Goats, Chickens, Turkeys, Cows, and Dogs!');
 
-// questionFour();
+  } else {
+    alert('Please try your answer again');
+    questionThree();
+  }
+
+}
+
+questionThree();
 
 
-// // Question 5: "Am I a good coder?"
+// Question 4: "Do I have a family?"
 
-// function questionFive() {
-//   let answer = prompt('Am I a good coder? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
+function questionFour() {
+  let answer = prompt('Do I have a family? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
 
-//   //   if (!answer) {
-//   //     alert('Please try your answer again');
-//   //     questionFive();
-//   //   }
+  //   if (!answer) {
+  //     alert('Please try your answer again');
+  //     questionFour();
+  //   }
 
-//   let answerLower;
+  let answerLower;
 
-//   if (answer) {
-//     answerLower = answer.toLowerCase();
-//   }
+  if (answer) {
+    answerLower = answer.toLowerCase();
+  }
+  if (answerLower === 'yes' || answerLower === 'y') {
+    // console.log('Answer yes: ' + answerLower);
+    alert('I sure do! I have an amazing wife and 4 children.');
 
-//   if (answerLower === 'yes' || answerLower === 'y') {
-//     // console.log('Answer yes: ' + answerLower);
-//     alert('You are too kind! This took forever to figure out!');
+  } else if (answerLower === 'no' || answerLower === 'n') {
+    console.log('Answer no: ' + answerLower);
+    alert('Wrong! I have an amazing wife and 4 children.');
 
-//   } else if (answerLower === 'no' || answerLower === 'n') {
-//     console.log('Answer no: ' + answerLower);
-//     alert('Ha well I also think I have a looooong way to go!');
+  } else {
+    alert('Please try your answer again');
+    questionFour();
+  }
 
-//   } else {
-//     alert('Please try your answer again');
-//     questionFive();
+}
 
-//   }
+questionFour();
 
-// }
 
-// questionFive();
+// Question 5: "Am I a good coder?"
 
-// alert('Thank you for taking the time ' + aName + ' to get to know me better. I hope you enjoy the rest of your time on this site! But first here are 2 more questions!');
+function questionFive() {
+  let answer = prompt('Am I a good coder? (Please answer "Yes", "No", "Y", or "N" in the input box below.');
+
+  //   if (!answer) {
+  //     alert('Please try your answer again');
+  //     questionFive();
+  //   }
+
+  let answerLower;
+
+  if (answer) {
+    answerLower = answer.toLowerCase();
+  }
+
+  if (answerLower === 'yes' || answerLower === 'y') {
+    // console.log('Answer yes: ' + answerLower);
+    alert('You are too kind! This took forever to figure out!');
+
+  } else if (answerLower === 'no' || answerLower === 'n') {
+    console.log('Answer no: ' + answerLower);
+    alert('Ha well I also think I have a looooong way to go!');
+
+  } else {
+    alert('Please try your answer again');
+    questionFive();
+
+  }
+
+}
+
+questionFive();
+
+alert('Thank you for taking the time ' + aName + ' to get to know me better. I hope you enjoy the rest of your time on this site! But first here are 2 more questions!');
 
 // Question 6: Guess a number game
 
-// let numOfGuesses = 4;
-// let correctNumber = 5;
+let numOfGuesses = 4;
+let correctNumber = 5;
 
 
-// while (numOfGuesses) {
+while (numOfGuesses) {
 
-//   let userGuess = prompt('Guess what number I am thinking of? (1-10)');
-//   console.log(userGuess);
-//   let comparedNumber = parseInt(userGuess);
+  let userGuess = prompt('Guess what number I am thinking of? (1-10)');
+  console.log(userGuess);
+  let comparedNumber = parseInt(userGuess);
 
-//   if (comparedNumber === correctNumber) {
-//     alert(`Congratulations! You picked the number ${correctNumber}!`);
-//     break;
+  if (comparedNumber === correctNumber) {
+    alert(`Congratulations! You picked the number ${correctNumber}!`);
+    break;
 
-//   } else if (numOfGuesses === 1) {
-//     alert(`Attempts Up...the number was ${correctNumber}!`);
-//     break;
+  } else if (numOfGuesses === 1) {
+    alert(`Attempts Up...the number was ${correctNumber}!`);
+    break;
 
-//   } else if (comparedNumber > 5) {
-//     alert('Too high!');
-//     numOfGuesses--;
-//     alert(`You have ${numOfGuesses} left!`);
+  } else if (comparedNumber > 5) {
+    alert('Too high!');
+    numOfGuesses--;
+    alert(`You have ${numOfGuesses} left!`);
 
-//   } else {
-//     (comparedNumber < 5);
-//     alert('Too low!');
-//     numOfGuesses--;
-//     alert(`You have ${numOfGuesses} left!`);
+  } else {
+    (comparedNumber < 5);
+    alert('Too low!');
+    numOfGuesses--;
+    alert(`You have ${numOfGuesses} left!`);
 
-//   }
-// }
+  }
+}
 
 
 // Question 7: Multiple Answer Array Game
@@ -232,6 +232,7 @@ let aName = '';
 let userScore = 0;
 let attempts = 6;
 
+
 let foods = ['pizza', 'cookies', 'french fries', 'cake', 'salad', 'wings', 'pasta', 'ice cream', 'soup', 'chicken', 'steak', 'fish', 'shrimp', 'seafood', 'bbq'];
 let foodDisplay = foods.join(', ');
 
@@ -239,11 +240,12 @@ alert('On the following prompts, please guess some of my favorite foods! You wil
 
 // Start of Prompt Loop
 
-for (let i = 0; i < attempts; i++) {
+for (let i = 0; i < 6; i++) {
   console.log(`Attempt # ${attempts}`);
+  console.log(`Attempt i ${i}`);
 
   let userGuesses = prompt(`${attempts} foods left to guess!`);
-  console.log(userGuesses);
+  // console.log(userGuesses);
 
   // If what the User guess is IN the Food Array
 
@@ -251,6 +253,8 @@ for (let i = 0; i < attempts; i++) {
     alert(`Yes! ${userGuesses} is a favorite food!`);
     userScore++;
     attempts--;
+    foods = foods.filter(element => element !== userGuesses);
+    console.log(foods);
 
     // If the User guess is NOT in the Food Array
   } else {
